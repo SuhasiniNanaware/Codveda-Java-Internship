b404b188bfbcf2e6dfa2f9d4c4a798f848982f87
Suhasini Nanaware <suhasininanware19@gmail.com>
2026-03-06 15:38:59 +0530

    Resolve .gitignore merge conflict

--- src/com/codveda/level1/Main.java
+++
@@ -1,8 +0,0 @@
-package com.codveda.level1;
-
-public class Main {
-    public static void main(String[] args) {
-        NumberGuessingGame game = new NumberGuessingGame();
-        game.startGame();
-    }
-}
